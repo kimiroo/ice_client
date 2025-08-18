1c8eb7252581a0351a8e8bb0d27355058aedc9e4
kimiroo <43537287+kimiroo@users.noreply.github.com>
2025-08-18 23:23:41 +0900

    Updated launch script

--- run.bat
+++ run.bat
@@ -1,19 +1,33 @@
 @echo off
 setlocal
-pushd ~dp0
 
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 
 if '%errorlevel%' NEQ '0' (
     echo Requesting administrative privileges...
-    goto UACPrompt
+    goto PreUACPrompt
 ) else ( goto gotAdmin )
 
-:UACPrompt
+:PreUACPrompt
+WHERE wt.exe >nul 2>&1
+if '%errorlevel%' NEQ '0' (
+    goto UACPrompt_CMD
+) else (
+    goto UACPrompt_WT
+)
+
+:UACPrompt_CMD
     echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
     set params = %*:"=""
     echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
 
+:UACPrompt_WT
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "wt.exe", "cmd.exe /c \""%~s0\"" %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
     "%temp%\getadmin.vbs"
     del "%temp%\getadmin.vbs"
     exit /B
@@ -22,14 +36,11 @@ if '%errorlevel%' NEQ '0' (
     pushd "%CD%"
     CD /D "%~dp0"
 
-set "VENV_DIR=.venv"
-set "ACTIVATE_SCRIPT=%VENV_DIR%\Scripts\activate.bat"
-
 REM Check if the virtual environment directory exists
-if not exist "%VENV_DIR%" (
+if not exist "%~dp0.venv\Scripts\activate.bat" (
     echo Virtual environment not found. Creating a new one...
-    python -m venv "%VENV_DIR%"
-    if not exist "%PYTHON_EXE%" (
+    python -m venv .venv
+    if not exist "%~dp0.venv\Scripts\activate.bat" (
         echo Error: Failed to create virtual environment.
         goto :EOF
     )
@@ -38,7 +49,7 @@ if not exist "%VENV_DIR%" (
 
 REM Activate the virtual environment
 echo Activating virtual environment...
-call "%ACTIVATE_SCRIPT%"
+call %~dp0.venv\Scripts\activate.bat
 
 REM Check if dependencies need to be installed or updated
 echo Installing dependencies...
